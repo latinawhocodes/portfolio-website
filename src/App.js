22efeffcd466bc506abdc6324d7ed3298d315b0a
Alyssa <46027209+latinawhocodes@users.noreply.github.com>
2022-11-02 08:59:44 -0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import './App.css';
+import { Header } from './components/Header';
 
 const App = () => {
   return (
     <div id="app" className="App">
-        Hello World
+        <Header></Header>
     </div>
   );
 }
