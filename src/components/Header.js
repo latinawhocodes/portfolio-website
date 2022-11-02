3bf26699fb86106d56fb97bd78a40feb4860dec0
Alyssa <46027209+latinawhocodes@users.noreply.github.com>
2022-11-02 08:58:13 -0700

    Update Header.js

---
+++ src/components/Header.js
@@ -0,0 +1,19 @@
+
+
+export const Header = () => {
+
+    return(
+        <>
+           <div className="header">
+                <div className="header-logo">
+                    <a href="#app"><h3 className="header-name">Alyssa Yesilyurt</h3></a>
+                </div>
+                <div className="header-container">
+                    <div className="nav-item"><a href="#about">About</a></div>
+                    <div className="nav-item"><a href="#projects">Projects</a></div>
+                    <div className="nav-item"><a href="#skills">Skills</a></div>
+                </div>
+        </div> 
+        </>
+    )
+}
